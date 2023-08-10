8fc0793603d0dc5706fec2e10e5d9249c8e436b5
jmpurvis0 <107958250+jmpurvis0@users.noreply.github.com>
2023-08-10 17:23:23 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,16 @@
-# add your code here
+num = 1
 
+while num <= 100:
+
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
+    num += 1
+
+print("Mission complete!")
